cbd211eb083a338aa14ba4a68bd0fdd6b3ec6e4e
brentaharris <85082559+brentaharris@users.noreply.github.com>
2022-01-05 12:52:07 -0500

    updated modals

--- main.js
+++ main.js
@@ -9,30 +9,55 @@ import { outsideGrid } from "./grid.js";
 
 import { update as updateFood, draw as drawFood } from "./food.js";
 
+//todo
+//implement difficulty and score
+
 let lastRenderTime = 0;
 let gameOver = false;
+let gameStarted = false;
 const gameBoard = document.getElementById("game-board");
+const playBtn = document.querySelector(".btn-play");
+const restartBtn = document.querySelector(".btn-restart");
+const modalEl = document.querySelector(".modal-start");
+const modalEndEl = document.querySelector(".modal-end");
 
+playBtn.addEventListener("click", startGame);
+restartBtn.addEventListener("click", restart);
 //main game loop
 function main(currentTime) {
-  if (gameOver) {
-    if (confirm("You lost, press OK to restart game.")) {
-      window.location = "/";
+  if (gameStarted) {
+    if (gameOver) {
+      // modalEndEl.classList.toggle("hidden");
+      if (confirm("You lost, press OK to restart game.")) {
+        window.location = "/";
+      }
+      return;
     }
-    return;
+    window.requestAnimationFrame(main);
+    let secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
+
+    if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
+
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
   }
-  window.requestAnimationFrame(main);
-  let secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
-
-  if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
-
-  lastRenderTime = currentTime;
-
-  update();
-  draw();
 }
 
 window.requestAnimationFrame(main);
+
+function startGame(difficulty) {
+  gameStarted = true;
+  modalEl.classList.toggle("hidden");
+  main();
+}
+
+function restart() {
+  modalEndEl.classList.toggle("hidden");
+  modalEl.classList.toggle("hidden");
+  console.log("click");
+}
 
 function update() {
   updateSnake();
