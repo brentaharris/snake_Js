1df6a050af2e5e038a74115ec6774bfa9e03520a
brentaharris <85082559+brentaharris@users.noreply.github.com>
2022-01-05 19:49:50 -0500

    updated score and how-to

--- howto.js
+++ howto.js
@@ -1,8 +1,8 @@
-Classic Snake Game coded by Brent. 
+export const howText = `Classic Snake Game coded by Brent. 
 Control the snake (colored in blue) with the keyboard arrow keys, up, down, left, or right. Your goal is to consume the yellow colored food (by contacting the 'head' of the snake with the food). Each time the food is consumed, the food will randomly re-spawn on the game board, and your snake will grow in length by one.
 Watch out! Do not run the snake into itself or the edges of the gameboard or you will lose!
 Difficulty: 
 Easy - snake speed is slow.
 Harder - snake speed is faster.
 Hardest - snake speed is VERY FAST.
-Have fun!
+Have fun!`;
--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import {
   snakeIntersection,
 } from "./snake.js";
 
+import { howText } from "./howto.js";
+
 import { outsideGrid } from "./grid.js";
 
 import { update as updateFood, draw as drawFood } from "./food.js";
@@ -21,10 +23,15 @@ const restartBtn = document.querySelector(".btn-restart");
 const modalEl = document.querySelector(".modal-start");
 const modalEndEl = document.querySelector(".modal-end");
 let difficultyEl = document.querySelector(".difficulty");
+const howBtn = document.querySelector(".btn-how-to");
+const howModal = document.querySelector(".modal-how-to");
+const closeHowBtn = document.querySelector(".btn-close");
 
 //event listeners
 playBtn.addEventListener("click", startGame);
 restartBtn.addEventListener("click", restart);
+howBtn.addEventListener("click", showHowTo);
+closeHowBtn.addEventListener("click", closeHowTo);
 difficultyEl.addEventListener("change", () => {
   switch (difficultyEl.value) {
     case "Easy":
@@ -94,4 +101,14 @@ function draw() {
 
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
+
+function showHowTo() {
+  howModal.classList.remove("hidden");
+  const howTextEl = document.querySelector(".how-text");
+  howTextEl.textContent = howText;
+}
+
+function closeHowTo() {
+  howModal.classList.add("hidden");
 }
